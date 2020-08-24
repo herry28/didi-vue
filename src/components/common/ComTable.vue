816d7af4bddb037f52fe6959bb0a4888c4971540
zouhai <zouhai_@outlook.com>
2020-08-24 17:03:53 +0800

    'user页面（form，table组件的封装）'

--- src/components/common/ComTable.vue
+++ src/components/common/ComTable.vue
@@ -1,3 +1,66 @@
 <template>
-    <div>table</div>
+    <div class="com-table">
+        <el-table :data='tableData' stripe v-loading='config.loading'>
+            <el-table-column label="序号" width="85">
+            <template slot-scope="scope">
+                <span style="margin-left: 10px">{{ (config.page-1)*20+scope.$index+1 }}</span>
+            </template>
+            </el-table-column>
+             <el-table-column show-overflow-tooltip v-for='item in tableLabel' :key='item.prop' :label='item.label' >
+            <template slot-scope="scope">
+                <span style="margin-left: 10px">{{ scope.row[item.prop] }}</span>
+            </template>
+            </el-table-column>
+            <el-table-column label="操作" class="operate">
+                <template slot-scope="scope">
+                    <el-button
+                    size="mini"
+                    @click="handleEdit(scope.row)">编辑</el-button>
+                    <el-button
+                    size="mini"
+                    type="danger"
+                    @click="handleDelete(scope.row)">删除</el-button>
+                </template>
+             </el-table-column>
+           
+        </el-table>
+          <el-pagination
+            background
+            layout="prev, pager, next"
+            @current-change='changePage'
+            :current-page.sync='config.page'
+            :total="config.total">
+        </el-pagination>
+       
+    </div>
 </template>
+
+
+<script>
+export default {
+    props:{
+        tableData:Array,
+        tableLabel:Array,
+        config:Object
+    },
+    methods: {
+      handleEdit(row) {
+        this.$emit('edit',row)
+      },
+      handleDelete(index, row) {
+        console.log(index, row);
+      },
+      changePage(page){
+          this.$emit('changePage',page)
+      }
+    }
+}
+</script>
+
+
+<style>
+    .cell{
+        display: flex;
+        justify-content: space-between;
+    }
+</style>
--- src/views/User/User.vue
+++ src/views/User/User.vue
@@ -1,10 +1,23 @@
 <template>
-    <div class='userManage'>user路由组件
+    <div class='userManage'>
+        <!-- <el-dialog 
+        :visible.sync='isShow'
+        :title="operateType=='add'?'新增用户':'更新用户'">
+            <com-form :formLabel='operateFormLabel' :form='operateForm'></com-form>
+        </el-dialog> -->
         <div class="user-hd">
-            <com-form></com-form>
+             <el-button type='primary'>+ 新增</el-button>
+            <com-form inline :formLabel='formLabel' :form='searchForm'>
+                <el-button type='primary'>搜索</el-button>
+            </com-form>
         </div>
         <div class="user-con">
-            <com-table></com-table>
+            <com-table 
+            @changePage="getUserData"
+            @eidt='editUser'
+            :tableData='tableData' 
+            :tableLabel='tableLabel' 
+            :config='config'></com-table>
         </div>
     </div>
 </template>
@@ -14,9 +27,120 @@
 import ComForm from '../../components/common/ComForm'
 import ComTable from '../../components/common/ComTable'
 export default {
-    component:{
+    components:{
         ComForm,
         ComTable
+    },
+    data(){
+        return {
+            // isShow:false,
+            // operateType:'add',
+            // operateForm:{
+            //     name: '',
+            //     addr:'',
+            //     age:'',
+            //     birth:'',
+            //     sex: ''
+            // },
+            // operateFormLabel:[
+            //      {
+            //         prop:'name',
+            //         label:'姓名'
+            //     },
+            //     {
+            //         prop:'age',
+            //         label:'年龄'
+            //     },
+            //     {
+            //         prop:'sexLabel',
+            //         label:'性别'
+            //     },
+            //     {
+            //         prop:'birth',
+            //         label:'出生日期',
+            //         type:date
+            //     },
+            //     {
+            //         prop:'addr',
+            //         label:'地址'
+            //     },
+            // ],
+            searchForm:{
+                keyword:''
+            },
+            formLabel:[
+                {
+                    model:'keyword',
+                    label:''
+                }
+            ],
+            tableData:[],
+            tableLabel:[
+                {
+                    prop:'name',
+                    label:'姓名'
+                },
+                {
+                    prop:'age',
+                    label:'年龄'
+                },
+                {
+                    prop:'sexLabel',
+                    label:'性别'
+                },
+                {
+                    prop:'birth',
+                    label:'出生日期'
+                },
+                {
+                    prop:'addr',
+                    label:'地址'
+                },
+            ],
+            config:{
+                page:1,
+                total:30,
+                loading:false
+            }
+
+        }
+    },
+    methods:{
+        getUserData(){
+            this.config.loading=true
+            this.$http.get('/userApi/user/getUser',{
+                params:{
+                    page:this.config.page,
+                }
+            }).then(res=>{
+                this.tableData=res.data.list.map(item=>{
+                    item.sexLabel=item.sex===0?'女':'男'
+                    return item
+                })
+                this.config.total=res.data.count
+                this.config.loading=false
+            })
+        },
+        editUser(row){
+            this.operateType='edit'
+            this.isShow=true
+        }
+    },
+    created(){
+        this.getUserData()
     }
 }
 </script>
+
+<style>
+ .userManage{
+     height: 90%;
+     padding-bottom: 20px;
+     overflow: hidden;
+ }
+  .userManage .user-hd{
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+  }
+</style>
